0630e5a4fc93a1eee1593389c06d03e8fadf862e
Miguel Carrizo <miguelcarrizo@gmail.com>
2020-02-12 09:47:08 +0000

    HICE UNA SUMA

--- FrmHolaMundo.vb
+++ FrmHolaMundo.vb
@@ -1,7 +1,9 @@
-﻿Public Class FrmHolaMundo
+Public Class FrmHolaMundo
 
     Private Sub bHolaMundo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bHolaMundo.Click
         MessageBox.Show("HELLO WORLD", "MESSAGE")
         MessageBox.Show("BYE " & DEVELOPER, "MESSAGE")
+        Dim A AS Integer = 1 + 2
+
     End Sub
 End Class
